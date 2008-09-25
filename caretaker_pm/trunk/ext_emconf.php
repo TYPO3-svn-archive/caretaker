d98da5fd3bce75308d55d4f147c3eb46778f2a15
martoro <martoro@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-09-25 11:28:54 +0000

    - moved instance and group flexinfo to new caretaker_pm extension
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/caretaker/trunk@12304 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ caretaker_pm/trunk/ext_emconf.php
@@ -0,0 +1,44 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: "caretaker_pm"
+#
+# Auto generated 25-09-2008 13:14
+#
+# Manual updates:
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Caretaker Project Management',
+	'description' => 'add PM related Features to Caretaker',
+	'category' => '',
+	'author' => 'Martin Ficzel',
+	'author_email' => 'ficzel@work.de',
+	'shy' => '',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.0.0',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:8:{s:9:"ChangeLog";s:4:"88ec";s:10:"README.txt";s:4:"9fa9";s:12:"ext_icon.gif";s:4:"1bdc";s:14:"ext_tables.php";s:4:"7878";s:14:"ext_tables.sql";s:4:"8076";s:16:"locallang_db.xml";s:4:"bb5d";s:19:"doc/wizard_form.dat";s:4:"4000";s:20:"doc/wizard_form.html";s:4:"f776";}',
+);
+
+?>
---
+++ caretaker_pm/trunk/ext_tables.sql
@@ -0,0 +1,15 @@
+#
+# Table structure for table 'tx_caretaker_instance'
+#
+CREATE TABLE tx_caretaker_instance (
+	tx_caretakerpm_flexinfo text NOT NULL
+);
+
+
+
+#
+# Table structure for table 'tx_caretaker_group'
+#
+CREATE TABLE tx_caretaker_group (
+	tx_caretakerpm_flexinfo text NOT NULL
+);
