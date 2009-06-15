c03c5ecb37bff65b8c4e7b5a44f57dadeca53d8b
thommyhh <thommyhh@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-06-15 12:35:44 +0000

    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/caretaker/trunk@21408 735d13b6-9817-0410-8766-e36946ffe9aa

--- caretaker_selenium/trunk/services/class.tx_caretakerseleniumTestService.php
+++ caretaker_selenium/trunk/services/class.tx_caretakerseleniumTestService.php
@@ -91,7 +91,7 @@ class tx_caretakerseleniumTestService extends tx_caretaker_TestServiceBase {
 				
 		$commands     = $this->getConfigValue('selenium_configuration');
 		
-		echo $commands;
+		//echo $commands;
 		
 		$error_time   = $this->getConfigValue('response_time_error');
 		$warning_time = $this->getConfigValue('response_time_warning');
