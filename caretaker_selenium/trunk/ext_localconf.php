c75e1f258796e4df767356c2988e6e36b17f3f0c
martoro <martoro@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-10-27 10:20:17 +0000

    moved service-registration from ext_tables to ext_localconf
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/caretaker/trunk@25892 735d13b6-9817-0410-8766-e36946ffe9aa

--- caretaker_selenium/trunk/ext_localconf.php
+++ caretaker_selenium/trunk/ext_localconf.php
@@ -2,4 +2,9 @@
 
 if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
 
+	// load Service Helper
+include_once(t3lib_extMgm::extPath('caretaker').'classes/class.tx_caretaker_ServiceHelper.php');
+tx_caretaker_ServiceHelper::registerCaretakerService ($_EXTKEY , 'services' , 'tx_caretakerselenium'   ,'Selenium Test', 'Run A Selenium Test' );
+
+
 ?>
