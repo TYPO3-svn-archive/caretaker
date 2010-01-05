d62a4333f7b416dff62a1037b45729ed5d970e1a
martoro <martoro@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-01-05 09:43:29 +0000

    fixed inclusion of caretaker service helper in caretaker_analyzer
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/caretaker/trunk@28390 735d13b6-9817-0410-8766-e36946ffe9aa

--- caretaker_analyzer/trunk/ext_tables.php
+++ caretaker_analyzer/trunk/ext_tables.php
@@ -20,7 +20,7 @@ $TCA['tx_caretakerselenium_server'] = array (
 );
 
 	// load Service Helper
-include_once(t3lib_extMgm::extPath('caretaker').'classes/class.tx_caretaker_ServiceHelper.php');
+include_once(t3lib_extMgm::extPath('caretaker').'classes/helpers/class.tx_caretaker_ServiceHelper.php');
 
 	// register Tests
 tx_caretaker_ServiceHelper::registerCaretakerService ($_EXTKEY , 'services' , 'tx_caretakeranalyzer'   ,'Logefile Analyzer', 'Find specific parts in any log file.' );
