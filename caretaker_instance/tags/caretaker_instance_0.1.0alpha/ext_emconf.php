f50fe5c61b1056bec232fd8fe44abadd1457b1b4
matrikz <matrikz@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-05-21 03:44:36 +0000

    * Tagged alpha versions of caretaker and caretaker_instances, Raised both versions to 0.2.0
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/caretaker/trunk@33342 735d13b6-9817-0410-8766-e36946ffe9aa

--- caretaker_instance/tags/caretaker_instance_0.1.0alpha/ext_emconf.php
+++ caretaker_instance/tags/caretaker_instance_0.1.0alpha/ext_emconf.php
@@ -1,9 +1,9 @@
 <?php
 
 ########################################################################
-# Extension Manager/Repository config file for ext: "caretaker"
+# Extension Manager/Repository config file for ext: "caretaker_instance"
 #
-# Auto generated 27-08-2008 08:58
+# Auto generated 27-08-2008 08:59
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -11,41 +11,39 @@
 ########################################################################
 
 $EM_CONF[$_EXTKEY] = array(
-	'title' => 'Caretaker',
-	'description' => 'Server for caretaker observation system',
+	'title' => 'Caretaker Instance',
+	'description' => 'Client for caretaker observation system',
 	'category' => 'misc',
 	'author' => 'Martin Ficzel,Thomas Hempel,Christopher Hlubek,Tobias Liebig',
 	'author_email' => 'ficzel@work.de,hempel@work.de,hlubek@networkteam.com,liebig@networkteam.com',
 	'shy' => '',
-	'dependencies' => 'cms,tt_address',
+	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
 	'module' => '',
 	'state' => 'alpha',
 	'internal' => '',
 	'uploadfolder' => 0,
-	'createDirs' => 'typo3temp/caretaker/charts',
+	'createDirs' => '',
 	'modify_tables' => '',
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
-	'TYPO3_version' => '4.3.0dev-',
-	'PHP_version' => '5.2-',
 	'author_company' => '',
-	'version' => '0.2.0',
+	'version' => '0.1.0alpha',
+	'TYPO3_version' => '4.0.0-',
+	'PHP_version' => '5.0.0-',
 	'constraints' => array(
 		'depends' => array(
 			'cms' => '',
-			'tt_address' => '2.2.1-',
-			'typo3' => '4.3.0dev-',
-			'php' => '5.2.0-'
+			'typo3' => '4.0.0-',
+			'php' => '5.0.0-'
 		),
 		'conflicts' => array(
 		),
 		'suggests' => array(
-			'css_styled_content' => '',
 		),
 	),
-	'_md5_values_when_last_written' => 'a:5:{s:9:"ChangeLog";s:4:"9c48";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:19:"doc/wizard_form.dat";s:4:"c49e";s:20:"doc/wizard_form.html";s:4:"c565";}',
+	'_md5_values_when_last_written' => 'a:5:{s:9:"ChangeLog";s:4:"9c48";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:19:"doc/wizard_form.dat";s:4:"9f12";s:20:"doc/wizard_form.html";s:4:"9378";}',
 );
 
 ?>
